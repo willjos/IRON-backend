cf86f43cb1bd22ed63f8b5b52aab5c35bd09cd14
Will <trainee.william.simms@sigmalabs.co.uk>
2023-01-26 18:56:45 +0000

    /login and /create-account functions working locally

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
 from database_functions import *
 import bcrypt
@@ -9,12 +9,33 @@ CORS(app)
 conn = get_db_connection()
 
 @app.route("/create-account", methods=["POST"])
-def register_user():
-    return
+def user_create_account():
+    data = request.json
+    username = data['username']
+    password = data['password'].encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password, salt).decode('utf-8')
+    query = "INSERT INTO users(username, hashedpw) VALUES (%s, %s)"
+    parameters = (username, hashed_password)
+    try:
+        db_insert(query, parameters)
+        return "Account Created", 200
+    except:
+        return "Failed to Create Account", 500
 
 @app.route("/login", methods=["GET"])
-def register_user():
-    return
+def user_login():
+    data =  request.json
+    username = data['username']
+    password = data['password']
+    query = "SELECT hashedpw FROM users WHERE username = %s"
+    parameters = (username,)    
+    user_data = db_fetch(query, parameters) 
+    hashed_password = user_data[0]['hashedpw']
+    if(bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))):
+        return "Access Granted", 200
+    else:
+        return "Access Denied", 403
 
 if __name__ == "__main__":
     app.run()
